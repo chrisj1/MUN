ad4f65a65de05bcb308bfb9f06b0e7c883cffad7
Chris Jerrett <cjerrett18@stjohnsprep.org>
2016-06-12 21:29:32 -0400

    created project and basic auth

---
+++ mun/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    <style type="text/css">
+        img {
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            width: 75%;
+        }
+    </style>
+
+    <div>
+        <p><h1 class="text-center">St. John's Prep Eleventh Annual Model United Nations Confrence</h1></p>
+    </div>
+
+    <div class="jumbotron" style="margin: 20px">
+        <img src="https://photos.smugmug.com/Student-Life/SJP-MUN-X/i-NvQ89kz/0/X2/IMG_6322-X2.jpg">
+          <h3 class="text-center">December 10, 2016</h3>
+           <h3 class="text-center">Registration opens September 7th</h3>
+          <p style="margin:5px">
+            
+          </p>
+    </div>
+@endsection

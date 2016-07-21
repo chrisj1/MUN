484a42bebb0c6721e11a56137fd1ed727099adb2
Chris Jerrett <cjerrett18@stjohnsprep.org>
2016-07-20 22:46:24 -0400

    Left this out in last sync

--- mun/app/Http/Controllers/DashboardController.php
+++ mun/app/Http/Controllers/DashboardController.php
@@ -11,10 +11,11 @@ use App\Http\Requests;
 
 use App\User;
 use App\Delegate;
+use App\Money;
 
 class DashboardController extends Controller
 {
-    public static $price_per_delegate = 7500;
+    public static $price_per_delegate = 75.00;
 
     /**
      * Create a new controller instance.
@@ -26,11 +27,13 @@ class DashboardController extends Controller
         $this->middleware('auth');
     }
 
-    public function index() {
+    public function index()
+    {
         return view('dashboard.index');
     }
 
-    public function manage() {
+    public function manage()
+    {
         $id = Auth::id();
         $user = Auth::user();
         $delegates = Delegate::where('user_id', '=', $id)->get();
@@ -43,7 +46,7 @@ class DashboardController extends Controller
         $lunches = Lunch::all();
 
         return view('dashboard.manage', ['delegates' => $delegates, 'user' => $user,
-            'delegate' => $delegate, 'committees' => $committees, 'lunches'=> $lunches]);
+            'delegate' => $delegate, 'committees' => $committees, 'lunches' => $lunches]);
     }
 
     public function addDelegate(Request $request)
@@ -63,14 +66,104 @@ class DashboardController extends Controller
         return back();
 
     }
-    
+
     public function payment()
     {
         $user = Auth::user();
+        $id = Auth::id();
         $paid = $user->amount_payed;
-        $amount_due = DashboardController::$price_per_delegate * count(Delegate::all()->where('user_id', '=', $user->id));
-        return view('dashboard.payment', ['paid'=>$paid, 'amount_due'=>$amount_due, 'price_per_delegate'=> money_format('%i', DashboardController::$price_per_delegate)]);
-    }
-    
+        $cost = DashboardController::$price_per_delegate * count(Delegate::where('user_id', '=', $id)->get());
 
+        return view('dashboard.payment', ['paid' => $paid, 'cost' => DashboardController::money_format('%#10n', $cost),
+            'price_per_delegate' => DashboardController::money_format(
+                '%#10n', DashboardController::$price_per_delegate),
+            'amount_due' => DashboardController::money_format('%#10n', $cost - $paid)]);
+    }
+
+    function money_format($format, $number)
+    {
+        $regex = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?' .
+            '(?:#([0-9]+))?(?:\.([0-9]+))?([in%])/';
+        if (setlocale(LC_MONETARY, 0) == 'C') {
+            setlocale(LC_MONETARY, '');
+        }
+        $locale = localeconv();
+        preg_match_all($regex, $format, $matches, PREG_SET_ORDER);
+        foreach ($matches as $fmatch) {
+            $value = floatval($number);
+            $flags = array(
+                'fillchar' => preg_match('/\=(.)/', $fmatch[1], $match) ?
+                    $match[1] : ' ',
+                'nogroup' => preg_match('/\^/', $fmatch[1]) > 0,
+                'usesignal' => preg_match('/\+|\(/', $fmatch[1], $match) ?
+                    $match[0] : '+',
+                'nosimbol' => preg_match('/\!/', $fmatch[1]) > 0,
+                'isleft' => preg_match('/\-/', $fmatch[1]) > 0
+            );
+            $width = trim($fmatch[2]) ? (int)$fmatch[2] : 0;
+            $left = trim($fmatch[3]) ? (int)$fmatch[3] : 0;
+            $right = trim($fmatch[4]) ? (int)$fmatch[4] : $locale['int_frac_digits'];
+            $conversion = $fmatch[5];
+
+            $positive = true;
+            if ($value < 0) {
+                $positive = false;
+                $value *= -1;
+            }
+            $letter = $positive ? 'p' : 'n';
+
+            $prefix = $suffix = $cprefix = $csuffix = $signal = '';
+
+            $signal = $positive ? $locale['positive_sign'] : $locale['negative_sign'];
+            switch (true) {
+                case $locale["{$letter}_sign_posn"] == 1 && $flags['usesignal'] == '+':
+                    $prefix = $signal;
+                    break;
+                case $locale["{$letter}_sign_posn"] == 2 && $flags['usesignal'] == '+':
+                    $suffix = $signal;
+                    break;
+                case $locale["{$letter}_sign_posn"] == 3 && $flags['usesignal'] == '+':
+                    $cprefix = $signal;
+                    break;
+                case $locale["{$letter}_sign_posn"] == 4 && $flags['usesignal'] == '+':
+                    $csuffix = $signal;
+                    break;
+                case $flags['usesignal'] == '(':
+                case $locale["{$letter}_sign_posn"] == 0:
+                    $prefix = '(';
+                    $suffix = ')';
+                    break;
+            }
+            if (!$flags['nosimbol']) {
+                $currency = $cprefix .
+                    ($conversion == 'i' ? $locale['int_curr_symbol'] : $locale['currency_symbol']) .
+                    $csuffix;
+            } else {
+                $currency = '';
+            }
+            $space = $locale["{$letter}_sep_by_space"] ? ' ' : '';
+
+            $value = number_format($value, $right, $locale['mon_decimal_point'],
+                $flags['nogroup'] ? '' : $locale['mon_thousands_sep']);
+            $value = @explode($locale['mon_decimal_point'], $value);
+
+            $n = strlen($prefix) + strlen($currency) + strlen($value[0]);
+            if ($left > 0 && $left > $n) {
+                $value[0] = str_repeat($flags['fillchar'], $left - $n) . $value[0];
+            }
+            $value = implode($locale['mon_decimal_point'], $value);
+            if ($locale["{$letter}_cs_precedes"]) {
+                $value = $prefix . $currency . $space . $value . $suffix;
+            } else {
+                $value = $prefix . $value . $space . $currency . $suffix;
+            }
+            if ($width > 0) {
+                $value = str_pad($value, $width, $flags['fillchar'], $flags['isleft'] ?
+                    STR_PAD_RIGHT : STR_PAD_LEFT);
+            }
+
+            $format = str_replace($fmatch[0], $value, $format);
+        }
+        return $format;
+    }
 }
